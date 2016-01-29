4f410033f10e175ad8f6ce509d95bfa96b18c8a6
rahul <rgaikwad@mail.csuchico.edu>
2016-01-29 11:05:18 -0800

    GameDie.cpp: Implemented roll method successfully

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,11 +1,16 @@
+#include <iostream>
 #include <cstdlib>
-
+#include <ctime>
+//#include "GameDie.h"
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
 }
 
-//generate a random number between 1-6 (inclusive) and return it
-int GameDie::roll()
+//generate a random number between 1-6 (inclusive) and display it
+void GameDie::roll()
 {
+    int score=(rand() %6)+1;
+    cout<<"dice score is "<< score<<endl;
 }
