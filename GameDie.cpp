b807e566f2cffae35a936ddc76b67abc552df1cf
rahul <rgaikwad@mail.csuchico.edu>
2016-02-08 10:38:18 -0800

    Modify roll display with dies size in between 4 and 20

--- GameDie.cpp
+++ GameDie.cpp
@@ -11,7 +11,12 @@ GameDie::GameDie()
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
+    int die_size;
+    cout<<"Enter die side in between 4 and 20 : ";
+    cin>>die_size;
+    cout<<endl;
     srand(static_cast<unsigned int>(time(NULL)%1000));
-    int score=(rand() %6)+1;
+    //int score=(rand() %6)+1;//code for 1 and 6 die
+    int score=(rand() % die_size)+1;
     return score;
 }
